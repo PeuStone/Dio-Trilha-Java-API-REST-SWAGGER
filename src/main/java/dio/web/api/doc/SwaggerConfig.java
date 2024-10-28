368117a81b4ca65c9d777f1f07b2fcc6025040e9
PeuStone <pedrohmotaaa@gmail.com>
2024-10-28 15:22:15 -0300

    Criando a documentação da API com o Swagger

---
+++ src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package dio.web.api.doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private Contact contato() {
+        return new Contact(
+            "Nome do projeto",
+                "http://localhost:8080",
+                "seuemail@.com.br"
+        );
+    }
+
+    private ApiInfoBuilder informacoesApi(){
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Titulo - REST API");
+        apiInfoBuilder.description("API de exemplo feita com springboot");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("licença da empresa");
+        apiInfoBuilder.licenseUrl("http://localhost:8080");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+    }
+
+    @Bean
+    public Docket detalheApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("dio.web.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informacoesApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+        return docket;
+    }
+}
